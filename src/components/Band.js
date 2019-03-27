456bbdb0017175bfa7bc881687d422b989ac5511
Samuel Cornet <samuel@noukod.com>
2019-03-27 12:51:55 +0000

    Automatically backed up by Learn

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,12 +1,13 @@
+
+export default Band;
+
 import React, { Component } from 'react';
 
 class Band extends Component {
 
   render() {
     return(
-      <div>
-        Band Component
-      </div>
+      <li>{this.props.band.name}<button onClick={() => this.props.deleteBand(this.props.band.id)}>DELETE</button></li>
     );
   }
 };
